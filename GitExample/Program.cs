cf725933c61859ed468a84f6d55b7cdf5355d203
Sadman Walid <40543547+SadmanWalid@users.noreply.github.com>
2022-07-17 10:22:21 +0600

    initial commit

---
+++ GitExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace GitExample
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there");
+        }
+    }
+
+
+}
